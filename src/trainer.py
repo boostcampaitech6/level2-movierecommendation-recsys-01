3d44b710b6ac7f40921ceb42dfc9240bf88a9ff0
GangBean <xbox2537@gmail.com>
2024-02-02 12:09:17 +0000

    feat: Trainer validate 구현 #2

--- src/trainer.py
+++ src/trainer.py
@@ -6,10 +6,13 @@ trainer.py
 - train (only loss)
 - valid (metric)
 '''
+import numpy as np
+import pandas as pd
+
 from tqdm import tqdm
 import torch
-from .models.FMModels import FM
 from .models.DeepFMModels import DeepFM
+from .metrics import recall_at_k, ndcg_k
 
 class Trainer():
     def __init__(self, args, cat_features_size) -> None:
@@ -23,7 +26,7 @@ class Trainer():
             raise Exception
         self.model.to(self.device)
         self.loss = torch.nn.BCELoss()
-      
+        self.cat_features_size = cat_features_size
       
     def get_model(self):
         return self.model
@@ -37,8 +40,7 @@ class Trainer():
         for epoch in range(self.args.epochs):
             train_loss = self.train(train_data_loader)
             valid_loss, valid_ndcg_k, valid_recall_k = self.validate(valid_data_loader)
-            print(f"epoch: {epoch+1} train_loss: {train_loss}, valid_loss: {valid_loss}, 
-                  valid_ndcg: {valid_ndcg_k}, valid_recall_k: {valid_recall_k}")
+            print(f"epoch: {epoch+1} train_loss: {train_loss}, valid_loss: {valid_loss}, valid_ndcg: {valid_ndcg_k}, valid_recall_k: {valid_recall_k}")
     
     
     def train(self, train_data_loader):
@@ -53,19 +55,56 @@ class Trainer():
             batch_loss.backward()
             self.optimizer.step()
             total_loss += batch_loss
+        
+        total_loss /= len(train_data_loader)
         return total_loss
             
         
     def validate(self, valid_data_loader):
         self.model.eval()
-        return None, None, None
+        valid_loss = 0
+        total_X = []
+        for i, data in tqdm(enumerate(valid_data_loader)):
+            X, y = data['X'].to(self.device), data['y'].to(self.device)
+            
+            pred = self.model(X)
+            batch_loss = self.loss(pred, y)
+            valid_loss += batch_loss
+
+            total_X.append(data['X'])
+
+        total_X = np.concatenate(total_X, axis=0)
+
+        valid_recall_k, valid_ndcg_k = self.evaluate(total_X)
+
+        valid_loss /= len(valid_data_loader)
+
+        return valid_loss, valid_recall_k, valid_ndcg_k
     
     
-    def evaluate(self):
+    # calcualte recall and ndcg
+    def evaluate(self, X, k=10):
         self.model.eval()
-        pass
-    
-    
-    
-    
-    
+        eval_recall_k = 0
+        eval_ndcg_k = 0
+
+        num_users = self.cat_features_size['user']
+        num_items = self.cat_features_size['item']
+
+        actual = pd.DataFrame(X, columns = ['user', 'item']).groupby('user')['item'].agg(set).sort_index().to_dict() # user별 사용한 item list
+
+        prediction = []
+        for user in tqdm(range(num_users)):
+            user_X = torch.tensor([[user, item] for item in range(num_items)], dtype=int).to(self.device)
+            user_pred = self.model(user_X).detach().cpu()
+            user_mask = torch.tensor([0 if item in actual[user] else 1 for item in range(num_items)], dtype=int)
+            
+            user_pred = user_pred.squeeze(1) * user_mask
+        
+            user_pred = np.argsort(user_pred.numpy())[::-1]
+            prediction.append(user_pred)
+
+        eval_recall_k = recall_at_k(list(actual.values()), prediction, k)
+        eval_ndcg_k = ndcg_k(list(actual.values()), prediction, k)
+
+        return eval_recall_k, eval_ndcg_k
