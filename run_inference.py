105cd73d9bc9ea2404807bc279e833aea37bb858
twndus <twndus1362@gmail.com>
2024-02-14 04:15:01 +0000

    fix: train fix를 inference에도 적용 #2

--- run_inference.py
+++ run_inference.py
@@ -11,8 +11,11 @@ import hydra
 from omegaconf import DictConfig
 
 from src.data.datasets import (
-    DataPipeline, FMDataset,
+    DataPipeline,
     )
+from src.data.FMdatasets import (
+    FMDataPipeline, FMDataset,
+    ) 
 from src.inference import Inferencer
 from src.utils import set_seed, create_data_path, save_submission
 
@@ -22,18 +25,30 @@ from torch.utils.data import DataLoader
 @hydra.main(config_path="./src/configs", config_name="inference_config", version_base='1.3')
 def main(args: DictConfig):
     # create data_path
-    data_path, train_path, valid_path = create_data_path(args)
-    data_pipeline = DataPipeline(args)
+    data_path, train_path, valid_path, evaluate_path = create_data_path(args)
+    if args.model_name in ('FM', 'DeepFM'):
+        data_pipeline = FMDataPipeline(args)
+    else:
+        raise ValueError()
 
     # 데이터 이미 있다고 가정
     train_data = data_pipeline.load_data(train_path)
     valid_data = data_pipeline.load_data(valid_path)
+    evaluate_data = data_pipeline.load_data(evaluate_path)
 
     # ordinal encoding
-    # cat_features = [name for name, options in args.feature_sets.items() if options == (1, 'C')]
-    cat_features = ['user', 'item']
+    print("encode categorical features...")
+    cat_features = [name for name, options in args.feature_sets.items() if options == [1, 'C']]
     train_data['X'] = data_pipeline.encode_categorical_features(train_data['X'], cat_features)
     valid_data['X'] = data_pipeline.encode_categorical_features(valid_data['X'], cat_features)
+    evaluate_data = data_pipeline.encode_categorical_features(evaluate_data, cat_features)
+
+    # scaling
+    print("scaling numeric features...")
+    num_features = [name for name, options in args.feature_sets.items() if options == [1, 'N']]
+    train_data['X'] = data_pipeline.scale_numeric_features(train_data['X'], num_features)
+    valid_data['X'] = data_pipeline.scale_numeric_features(valid_data['X'], num_features)
+    evaluate_data = data_pipeline.scale_numeric_features(evaluate_data, num_features)
     
     # dataset
     train_dataset = FMDataset(train_data, train=True)
@@ -44,7 +59,7 @@ def main(args: DictConfig):
     valid_dataloader = DataLoader(valid_dataset, batch_size=2048, shuffle=True)
 
     # Load Best Model
-    inferencer = Inferencer(args, data_pipeline.cat_features_size, train_dataloader, valid_dataloader)
+    inferencer = Inferencer(args, data_pipeline, train_dataloader, valid_dataloader, evaluate_data)
 
     # Inference
     prediction = inferencer.inference()
--- src/inference.py
+++ src/inference.py
@@ -9,38 +9,53 @@ from .models.FMModels import FM
 
 class Inferencer:
 
-    def __init__(self, args, cat_features_size, train_dataloader, valid_dataloader):
+    def __init__(self, args, data_pipeline, train_dataloader, valid_dataloader, evaluate_data):
 
         self.args = args
         self.device = torch.device("cuda")
-        self.cat_features_size = cat_features_size
+        self.num_features = data_pipeline.num_features
+        self.cat_features_size = data_pipeline.cat_features_size
 
         self.model = None
         self.train_actual = None
         self.valid_actual = None
 
         self._load_model()
+
+        self.train_actual = None
+        self.valid_actual = None
         self._set_actuals(train_dataloader, valid_dataloader)
 
+        self.total_interaction = torch.tensor(evaluate_data.values).to(self.device)
+
     def _actual_interaction_dict(self, X):
-        return pd.DataFrame(X, columns = ['user', 'item']).groupby('user')['item'].agg(set).sort_index().to_dict()
+        offset = len(self.num_features)
+        return pd.DataFrame(X[:, offset:offset+2], columns = ['user', 'item']).groupby('user')['item'].agg(set).sort_index().to_dict()
 
     def _set_actuals(self, train_dataloader, valid_dataloader):
         print('set actuals...')
 
-        self.model.eval()
-        train_X = np.concatenate([data['X'] for data in train_dataloader], axis=0)
-        valid_X = np.concatenate([data['X'] for data in valid_dataloader], axis=0)
+        train_pos_X, valid_pos_X = [], []
+        for data in train_dataloader:
+            positive_index = torch.where(data['y'][:,0] == 1)
+            train_pos_X.append(data['X'][positive_index])
 
-        self.train_actual = self._actual_interaction_dict(train_X)
-        self.valid_actual = self._actual_interaction_dict(valid_X)
+        for data in train_dataloader:
+            positive_index = torch.where(data['y'][:,0] == 1)
+            valid_pos_X.append(data['X'][positive_index])
+
+        train_pos_X = np.concatenate(train_pos_X, axis=0)
+        valid_pos_X = np.concatenate(valid_pos_X, axis=0)
+
+        self.train_actual = self._actual_interaction_dict(train_pos_X)
+        self.valid_actual = self._actual_interaction_dict(valid_pos_X)
 
     def _load_model(self):
-        print('load model...')
+        print('load best model...')
         if self.args.model_name == "FM":
-            self.model = FM(self.cat_features_size, self.args.emb_dim)
+            self.model = FM(self.num_features, self.cat_features_size, self.args.emb_dim)
         elif self.args.model_name == "DeepFM":
-            self.model = DeepFM(self.cat_features_size, self.args.emb_dim, mlp_dims=[200, 200, 200], drop_rate=0.1)
+            self.model = DeepFM(self.num_features, self.cat_features_size, self.args.emb_dim, mlp_dims=[200, 200, 200], drop_rate=0.1)
         else:
             raise Exception
 
@@ -57,26 +72,34 @@ class Inferencer:
 
     def inference(self, k=10):
         print("Inference Start....")
+        self.model.eval()
 
         num_users = self.cat_features_size['user']
         num_items = self.cat_features_size['item']
+        offset = len(self.num_features)
         prediction = []
 
         print("Predict all users and items interaction....")
-        for _, user in enumerate(tqdm(range(num_users))):
-            user_X = torch.tensor([[user, item] for item in range(num_items)], dtype=int).to(self.device)
+        users = self.total_interaction[:, offset].unique().detach().cpu().numpy()
+        for idx, user in enumerate(tqdm(users)):
+            start_idx, end_idx = idx * num_items, (idx+1) * num_items
+            user_X = self.total_interaction[start_idx:end_idx, :]
+            user_items = user_X.detach().cpu().numpy()[:, offset+1]
             user_mask = torch.tensor([0 if (
-                item in self.train_actual[user]) or (item in self.valid_actual[user]) else 1 for item in range(num_items)], dtype=int)
+                item.item() in self.train_actual[int(user)]) or (item.item() in self.valid_actual[int(user)]) else 1 for item in user_items], dtype=int)
             
-            user_pred = self.model(user_X).detach().cpu()
+            user_pred = self.model(user_X.float()).detach().cpu()
             user_pred = user_pred.squeeze(1) * user_mask # train interaction 제외
             
-            user_pred = np.argpartition(user_pred.numpy(), -k)[-k:]
-            prediction.append(user_pred)
+            # find high prob index
+            high_index = np.argpartition(user_pred.numpy(), -k)[-k:]
+            # find high prob item by index
+            user_recom = user_items[high_index]
+            prediction.append(user_recom)
         
         # expand_dims
         prediction = np.expand_dims(np.concatenate(prediction, axis=0), axis=-1)
-        user_ids = np.expand_dims(np.repeat(np.arange(num_users), 10), axis=-1)
+        user_ids = np.expand_dims(np.repeat(users, 10), axis=-1).astype(int)
 
         prediction = np.concatenate([user_ids, prediction], axis=1)
 
